b5c55867dc3462c26caeb2636725c0292b87bec9
Mateus Castro <matcastro>
2018-05-27 23:42:25 -0300

    Alura - Projeto curso Spring MVC

---
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/CarrinhoComprasController.java
@@ -0,0 +1,60 @@
+package br.com.casadocodigo.loja.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.com.casadocodigo.loja.daos.ProdutoDAO;
+import br.com.casadocodigo.loja.models.CarrinhoCompras;
+import br.com.casadocodigo.loja.models.CarrinhoItem;
+import br.com.casadocodigo.loja.models.Produto;
+import br.com.casadocodigo.loja.models.TipoPreco;
+
+@Controller
+@RequestMapping("/carrinho")
+@Scope(value=WebApplicationContext.SCOPE_REQUEST)
+public class CarrinhoComprasController {
+
+	@Autowired
+	ProdutoDAO produtoDAO;
+	
+	@Autowired
+	CarrinhoCompras carrinhoCompras;
+	
+	@RequestMapping("/add")
+	public ModelAndView add(Integer produtoId, TipoPreco tipoPreco) {
+		ModelAndView modelAndView = new ModelAndView("redirect:/carrinho");
+		
+		CarrinhoItem carrinhoItem = criaItem(produtoId, tipoPreco);
+		carrinhoCompras.add(carrinhoItem);
+		
+		System.out.println(carrinhoCompras.getQuantidade());
+		
+		return modelAndView;
+	}
+
+	@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView itens() {
+		ModelAndView modelAndView = new ModelAndView("/carrinho/itens");
+		return modelAndView;
+	}
+	
+	@RequestMapping("/remover")
+	public ModelAndView remover(Integer produtoId, TipoPreco tipo, RedirectAttributes model) {
+		Produto produto = new Produto();
+		produto.setId(produtoId);
+		CarrinhoItem item = new CarrinhoItem(produto, tipo);
+		carrinhoCompras.remover(item);
+		return new ModelAndView("/carrinho/itens");
+	}
+	
+	private CarrinhoItem criaItem(Integer produtoId, TipoPreco tipoPreco) {
+		Produto produto = produtoDAO.find(produtoId);
+		return new CarrinhoItem(produto, tipoPreco);
+	}
+}
---
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -0,0 +1,73 @@
+package br.com.casadocodigo.loja.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.com.casadocodigo.loja.daos.ProdutoDAO;
+import br.com.casadocodigo.loja.infra.FileSaver;
+import br.com.casadocodigo.loja.models.Produto;
+import br.com.casadocodigo.loja.models.TipoPreco;
+import br.com.casadocodigo.loja.validation.ProdutoValidation;
+
+@Controller
+@RequestMapping("/produtos")
+public class ProdutosController {
+
+	@Autowired
+	private ProdutoDAO produtoDao;
+
+	@Autowired
+	private FileSaver saver;
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(new ProdutoValidation());
+	}
+	
+	@RequestMapping("/form")
+	public ModelAndView form(Produto produto) {
+		ModelAndView modelAndView = new ModelAndView("produtos/form");
+		modelAndView.addObject("tipos", TipoPreco.values());
+		return modelAndView;
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ModelAndView gravar(MultipartFile sumario,@Valid Produto produto, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+		if(bindingResult.hasErrors()) {
+			return form(produto);
+		}
+		
+		String path = saver.write("arquivos-sumario", sumario);
+		produto.setSumarioPath(path);
+		produtoDao.gravar(produto);
+		redirectAttributes.addFlashAttribute("sucesso", "Produto adicionado com sucesso!");
+		return new ModelAndView("redirect:produtos");
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView listar(Produto produto) {
+		ModelAndView modelAndView = new ModelAndView("produtos/lista");
+		modelAndView.addObject("produtos", produtoDao.listar());
+		return modelAndView;
+	}
+	
+	@RequestMapping("/detalhe/{id}")
+	public ModelAndView detalhe(@PathVariable("id") Integer id){
+		ModelAndView modelAndView = new ModelAndView("produtos/detalhe");
+		System.out.println(id);
+		Produto produto = produtoDao.find(id);
+		modelAndView.addObject("produto", produto);
+		return modelAndView;
+	}
+}
